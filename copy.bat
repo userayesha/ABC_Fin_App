5190584af44b254f4db5ffa7e740b90b35104059
sivakumarsagar <52564643+sivakumarsagar@users.noreply.github.com>
2020-03-19 11:43:07 -0400

    Create copy.bat

---
+++ copy.bat
@@ -0,0 +1,10 @@
+
+xcopy /s ABC_Fin_App\templates ./
+xcopy /s ABC_Fin_App\db_creation_script ./
+xcopy /s ABC_Fin_App\loan_app.py ./
+xcopy /s ABC_Fin_App\selinium1.py ./
+xcopy /s ABC_Fin_App\setupDB.py ./
+xcopy /s ABC_Fin_App\smoketest.py ./
+xcopy /s ABC_Fin_App\virtualenv.bat ./
+xcopy /s ABC_Fin_App\delete.bat ./
+xcopy /s ABC_Fin_App\copy.bat ./
