912642faed6231bc55eda6857b46042936836efb
sivakumarsagar <52564643+sivakumarsagar@users.noreply.github.com>
2020-03-20 00:26:29 -0400

    Update mergeDeveloper.bat

--- mergeDeveloper.bat
+++ mergeDeveloper.bat
@@ -3,7 +3,7 @@ git config --global user.email "kicksiva191964@gmail.com"
 //git checkout release
 cd ABC_Fin_App
 echo %CD%
-//git --git-dir=C:\Users\home1\.jenkins\workspace\pipeline\ABC_Fin_App checkout release
-//git --git-dir=C:\Users\home1\.jenkins\workspace\pipeline\ABC_Fin_App fetch origin
-//git --git-dir=C:\Users\home1\.jenkins\workspace\pipeline\ABC_Fin_App pull
-//git --git-dir=C:\Users\home1\.jenkins\workspace\pipeline\ABC_Fin_App merge origin/developer
+git  checkout release
+git fetch origin
+git pull
+git origin/developer
