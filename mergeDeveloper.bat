34984929e815d4d82bf36a8803024a7bf429830d
sivakumarsagar <52564643+sivakumarsagar@users.noreply.github.com>
2020-03-20 00:34:35 -0400

    Update mergeDeveloper.bat

--- mergeDeveloper.bat
+++ mergeDeveloper.bat
@@ -1,9 +1,7 @@
 git config --global user.name "sivakumarsagar"
 git config --global user.email "kicksiva191964@gmail.com"
-//git checkout release
-cd ABC_Fin_App
-echo %CD%
 git  checkout release
 git fetch origin
 git pull
 git merge origin/developer
+git push
